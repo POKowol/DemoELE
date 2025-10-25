89a4a9dcae96de0c14ec5836f7a9c9f939e2fbf9
Marcin Kowol <m.kowol@po.edu.pl>
2025-10-25 08:57:35 +0200

    Dodaj nowy plik 'zadanie_MSR.py' z podstawową strukturą aplikacji Tkinter.
    dodałem nowy plik 'zadanie_MSR.py' w którym znajduje się podstawowa struktura aplikacji Tkinter.
    nie dodałem żadnych dodatkowych plików ani folderów.

---
+++ zadanie_MSR.py
@@ -0,0 +1,9 @@
+import tkinter as tk  # Import the tkinter library for GUI development
+from tkinter import ttk  # Import the themed tkinter widgets
+
+root = tk.Tk()  # Create the main application window
+root.title("Zadanie 7 z maszyn elektryczynch")  # Set the title of the window
+root.geometry("400x300")  # Set the size of the window
+root.resizable(False, False)  # Disable window resizing
+
+root.mainloop()  # Start the Tkinter event loop
