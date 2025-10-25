a0ddd5ea76a8ea7ac90bb1a8d083e4c380a2ceb4
Marcin Kowol <m.kowol@po.edu.pl>
2025-10-25 09:24:30 +0200

    dodaj warotosc pola przekroju rdzenia
    dfdkjafl

--- zadanie_MSR.py
+++ zadanie_MSR.py
@@ -6,14 +6,39 @@ root.title("Zadanie 7 z maszyn elektryczynch")  # Set the title of the window
 root.geometry("400x300")  # Set the size of the window
 root.resizable(False, False)  # Disable window resizing
 
+Sf_var = tk.StringVar()  # Variable to store the cross-sectional area input
+Sf_var.set("0.0025")  # Set default value for cross-sectional area
+lf_var = tk.StringVar()  # Variable to store the average magnetic line length input
+lf_var.set("0.8")  # Set default value for average magnetic line length
+
 # Create a label widget
 label_Sf = ttk.Label(root, text="Pole przekroju redzenia:")
 label_Sf.grid(column=0, row=0, padx=10, pady=10, sticky='W')
+# Bind the label to the Sf_var variable
+
 
 entry_Sf = ttk.Entry(root)  # Create an entry widget for user input
 entry_Sf.grid(column=1, row=0, padx=10, pady=10)
+entry_Sf.configure(textvariable=Sf_var)  # Link the entry to Sf_var
 
 ttk.Label(root, text="m\u00B2").grid(column=2, row=0,
                                      padx=10, pady=10, sticky='W')  # Unit label
+
+# Create a label widget
+label_lf = ttk.Label(root, text="Średnia długość linii magnetycznej:")
+label_lf.grid(column=0, row=1, padx=10, pady=10, sticky='W')
+
+# Bind the label to the lf_var variable
+
+
+entry_lf = ttk.Entry(root)  # Create an entry widget for user input
+entry_lf.grid(column=1, row=1, padx=10, pady=10)
+entry_lf.configure(textvariable=lf_var)  # Link the entry to lf_var
+
+ttk.Label(root, text="m").grid(column=2, row=1,
+                               padx=10, pady=10, sticky='W')  # Unit label
+
+button_calculate = ttk.Button(root, text="Oblicz")  # Create a button widget
+button_calculate.grid(column=0, row=2, padx=10, pady=20, columnspan=3)
 
 root.mainloop()  # Start the Tkinter event loop
