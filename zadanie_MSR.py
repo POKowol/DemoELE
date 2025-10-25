5dcfbaf74d110b4bddea0be3b59ead5d4b85510c
Marcin Kowol <m.kowol@po.edu.pl>
2025-10-25 09:08:41 +0200

    dodaj widzety do okna głównego aplikacji tkinter
    dodalem pole tekstowe z etykietami "Pole przekroju rdzenia:" i "m²"

--- zadanie_MSR.py
+++ zadanie_MSR.py
@@ -6,4 +6,14 @@ root.title("Zadanie 7 z maszyn elektryczynch")  # Set the title of the window
 root.geometry("400x300")  # Set the size of the window
 root.resizable(False, False)  # Disable window resizing
 
+# Create a label widget
+label_Sf = ttk.Label(root, text="Pole przekroju redzenia:")
+label_Sf.grid(column=0, row=0, padx=10, pady=10, sticky='W')
+
+entry_Sf = ttk.Entry(root)  # Create an entry widget for user input
+entry_Sf.grid(column=1, row=0, padx=10, pady=10)
+
+ttk.Label(root, text="m\u00B2").grid(column=2, row=0,
+                                     padx=10, pady=10, sticky='W')  # Unit label
+
 root.mainloop()  # Start the Tkinter event loop
